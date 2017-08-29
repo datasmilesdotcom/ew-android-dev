b07644e0d5dda3b083e95160272ff44e56d7d2d5
Sandesh <sandeshlasnapure@gmail.com>
2017-08-29 13:11:41 +0530

    Explore Activity

--- app/src/main/java/app/mobile/examwarrior/demo/QuestionPagerActivity.java
+++ app/src/main/java/app/mobile/examwarrior/demo/QuestionPagerActivity.java
@@ -1,22 +1,19 @@
 package app.mobile.examwarrior.demo;
 
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import android.webkit.WebView;
 import android.widget.TextView;
 
 import app.mobile.examwarrior.R;
@@ -26,6 +23,7 @@ import de.timfreiheit.mathjax.android.MathJaxView;
 
 public class QuestionPagerActivity extends AppCompatActivity {
 
+    public static String[] que;
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
@@ -35,8 +33,6 @@ public class QuestionPagerActivity extends AppCompatActivity {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
-    public static String[] que;
-
     /**
      * The {@link ViewPager} that will host the section contents.
      */
@@ -266,7 +262,6 @@ public class QuestionPagerActivity extends AppCompatActivity {
                 ((CheckableLinearLayout)v).toggle();
 
             }
-
         }
     }
 
